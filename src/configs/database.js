95fee0572b66a0f165fe0986dac4cb6eca84f3f5
Nitzan <nitzantomer1998@gmail.com>
2023-06-21 16:48:57 +0300

    Adding database configuration

---
+++ src/configs/database.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  const connectOptions = {
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true,
+  };
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, connectOptions);
+
+    console.log('Database Connected');
+  } catch (error) {
+    console.error(`connectDB Error: ${error.message}`);
+  }
+};
+
+export default connectDB;
