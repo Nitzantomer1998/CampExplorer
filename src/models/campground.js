85c5f5db22e1685916758942ce8e46d4294e3787
Nitzan <nitzantomer1998@gmail.com>
2023-06-22 08:01:17 +0300

    Adding image field

--- src/models/campground.js
+++ src/models/campground.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const CampgroundSchema = new mongoose.Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String,
 });
