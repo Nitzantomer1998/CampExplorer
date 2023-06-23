e5dd8c825f917baa7860a622709bd8ca2c9908d7
Nitzan <nitzantomer1998@gmail.com>
2023-06-23 09:17:22 +0300

    Updating exports

--- src/middlewares/joiMiddleware.js
+++ src/middlewares/joiMiddleware.js
@@ -21,4 +21,27 @@ const joiReviewMiddleware = joi.object({
     .required(),
 });
 
-export { joiCampgroundMiddleware, joiReviewMiddleware };
+const validateCampground = (req, res, next) => {
+  console.log('here?');
+  const { error } = joiCampgroundMiddleware.validate(req.body);
+  console.log(error);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = joiReviewMiddleware.validate(req.body);
+
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+export { validateCampground, validateReview };
