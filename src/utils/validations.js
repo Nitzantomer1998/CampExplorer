857fda9b26261dc4bf15b23a0209716002f003d8
Nitzan <nitzantomer1998@gmail.com>
2023-06-23 09:18:34 +0300

    Adding validation script

---
+++ src/utils/validations.js
@@ -0,0 +1,29 @@
+import {
+  joiCampgroundMiddleware,
+  joiReviewMiddleware,
+} from '../middlewares/joiMiddleware.js';
+
+const validateCampground = (req, res, next) => {
+  console.log('here?');
+  const { error } = joiCampgroundMiddleware.validate(req.body);
+  console.log(error);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const { error } = joiReviewMiddleware.validate(req.body);
+
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+export { validateCampground, validateReview };
