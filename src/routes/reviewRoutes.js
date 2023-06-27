e1b88cddfdfe7e4eacc9357c8ac1b00097a2d9b3
Nitzan <nitzantomer1998@gmail.com>
2023-06-27 09:52:41 +0300

    Updating GET to POST

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import wrapAsync from '../utils/wrapAsync.js';
 import { validateReview } from '../utils/validations.js';
-import {
-  getAllReviews,
-  deleteReview,
-} from '../controllers/reviewController.js';
+import { createReview, deleteReview } from '../controllers/reviewController.js';
 
 const router = express.Router();
 
 router.post(
   '/campgrounds/:id/reviews',
   validateReview,
-  wrapAsync(getAllReviews)
+  wrapAsync(createReview)
 );
 
 router.delete('/campgrounds/:id/reviews/:reviewId', wrapAsync(deleteReview));
