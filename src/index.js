3a06ad8819ebe3cce19e9055419e5eb5390dfcff
Nitzan <nitzantomer1998@gmail.com>
2023-06-27 09:54:16 +0300

    Adding Flash & Session

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import fs from 'fs';
 import dotenv from 'dotenv';
 import express from 'express';
 import ejsMate from 'ejs-mate';
+import flash from 'connect-flash';
+import session from 'express-session';
 import methodOverride from 'method-override';
 import wrapAsync from './utils/wrapAsync.js';
 import connectDB from './configs/database.js';
-import ExpressError from './utils/ExpressError.js';
 
 dotenv.config();
 
@@ -19,19 +20,32 @@ app.set('views', 'src/views');
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + parseInt(process.env.EXPIRATION_TIME),
+    },
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.msg = req.flash('msg')[0];
+  next();
+});
 
-fs.readdirSync('./src/routes').map(
+fs.readdirSync('./src/routes').forEach(
   wrapAsync(async (route) =>
     app.use('/', (await import(`./routes/${route}`)).default)
   )
 );
 
-app.use((error, req, res, next) => {
+app.use((err, req, res, next) => {
+  const error = { statusCode: 404, message: 'Page Not Found' };
   res.render('error', { error });
 });
-
-// app.all('*', (req, res, next) => {
-//   next(new ExpressError('Page Not Found', 404));
-// });
 
 app.listen(process.env.PORT, () => console.log('Server Connected'));
