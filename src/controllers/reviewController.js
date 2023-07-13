519349983987f9db826e244ac0a2bb700f449f9c
Nitzan <nitzantomer1998@gmail.com>
2023-07-13 13:44:53 +0300

    Updating Function Names

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,15 +1,6 @@
 import Review from '../models/reviewModel.js';
 import Campground from '../models/campgroundModel.js';
 
-// const getAllReviews = async (req, res) => {
-//   const campground = await Campground.findById(req.params.id);
-//   const review = new Review(req.body.review);
-//   campground.reviews.push(review);
-//   await review.save();
-//   await campground.save();
-//   res.redirect(`/campgrounds/${campground._id}`);
-// };
-
 const createReview = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
