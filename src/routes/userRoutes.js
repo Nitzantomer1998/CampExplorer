bf6dbabbec6d542f6199374d33cbbcada95a5deb
Nitzan <nitzantomer1998@gmail.com>
2023-07-17 17:37:03 +0300

    Adding user Routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import wrapAsync from '../utils/wrapAsync.js';
+import { validateUser } from '../utils/validations.js';
+import {
+  getRegisterationPage,
+  getLoginPage,
+  registerUser,
+  loginUser,
+} from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.get('/register', wrapAsync(getRegisterationPage));
+router.get('/login', wrapAsync(getLoginPage));
+
+router.post('/register', validateUser, wrapAsync(registerUser));
+router.post('/login', wrapAsync(loginUser));
+
+export default router;
