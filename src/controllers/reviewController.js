f4a88d90bae7216015c5f7ecf82240e6b866bfde
Nitzan <nitzantomer1998@gmail.com>
2023-07-19 16:19:35 +0300

    Adding Authorization check

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -4,6 +4,7 @@ import Campground from '../models/campgroundModel.js';
 const createReview = async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
+  review.author = req.session.user_id;
   campground.reviews.push(review);
   await review.save();
   await campground.save();
