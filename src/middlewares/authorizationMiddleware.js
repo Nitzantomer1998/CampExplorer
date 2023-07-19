34363e686f56bdccaf6dc1a7feefb6d681e2fa9b
Nitzan <nitzantomer1998@gmail.com>
2023-07-19 16:15:10 +0300

    Adding Autherization Function

---
+++ src/middlewares/authorizationMiddleware.js
@@ -0,0 +1,52 @@
+import Campground from '../models/campgroundModel.js';
+
+const isLoggedIn = (req, res, next) => {
+  if (req.session.user_id) {
+    return next();
+  }
+
+  req.flash('msg', {
+    type: 'info',
+    message: 'Unauthorized Access, Please login first',
+  });
+
+  res.redirect('/login');
+};
+
+const isCampgroundAuthor = async (req, res, next) => {
+  const campground = await Campground.findById(req.params.id);
+  if (campground.author._id.toString() === req.session.user_id) {
+    return next();
+  }
+
+  req.flash('msg', {
+    type: 'info',
+    message: 'Unauthorized Access, You are not the author of this campground',
+  });
+
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+const isReviewAuthor = async (req, res, next) => {
+  const campground = await Campground.findById(req.params.id);
+  if (campground.author._id.toString() === req.session.user_id) {
+    return next();
+  }
+
+  const review = campground.reviews.find(
+    (review) => review._id.toString() === req.params.reviewId
+  );
+
+  if (review === req.session.user_id) {
+    return next();
+  }
+
+  req.flash('msg', {
+    type: 'info',
+    message: 'Unauthorized Access, You are not the author of this review',
+  });
+
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+export { isLoggedIn, isCampgroundAuthor, isReviewAuthor };
