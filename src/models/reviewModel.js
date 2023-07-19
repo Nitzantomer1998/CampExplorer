7f65ead0ad4ec47e8ee75a7277620875d93fe9d0
Nitzan <nitzantomer1998@gmail.com>
2023-07-19 16:21:14 +0300

    Adding author field

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 const reviewSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
   rating: Number,
   body: String,
 });
