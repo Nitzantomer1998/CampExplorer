f46a561aed49ee5e3a14d97ea2ad561f58698023
Nitzan <nitzantomer1998@gmail.com>
2023-07-20 16:09:28 +0300

    Adding cloudinary configuration

---
+++ src/configs/cloudinaryConfig.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const cloudinaryStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'CampExplorer',
+    allowedFormats: ['jpeg', 'jpg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  },
+});
+
+export default cloudinaryStorage;
