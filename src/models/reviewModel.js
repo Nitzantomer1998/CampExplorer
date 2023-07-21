5340f72a94d6135a60f30ab89e7cbb5cca5ffc5b
Nitzan <nitzantomer1998@gmail.com>
2023-07-21 20:27:02 +0300

    Updating restrictions

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 
 const reviewSchema = new mongoose.Schema({
+  rating: { type: Number, required: true },
+  body: { type: String, required: true },
+
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
   },
-  rating: { type: Number, required: true },
-  body: { type: String, required: true },
 });
 
 const Review = mongoose.model('Review', reviewSchema);
