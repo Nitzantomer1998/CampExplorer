711737adf3e3494a0c9edfc7596f732d8c675e5c
Nitzan <nitzantomer1998@gmail.com>
2023-07-21 20:36:21 +0300

    Updating Structure

--- src/middlewares/authorizationMiddleware.js
+++ src/middlewares/authorizationMiddleware.js
@@ -1,54 +1,53 @@
 import Campground from '../models/campgroundModel.js';
 
 const isLoggedIn = (req, res, next) => {
-  if (req.session.user_id) {
-    return next();
+  if (!req.session.user_id) {
+    req.flash('msg', {
+      type: 'info',
+      message: 'Unauthorized access, Login first',
+    });
+
+    res.redirect('/login');
   }
 
-  req.flash('msg', {
-    type: 'info',
-    message: 'Unauthorized access, Please login first',
-  });
-
-  res.redirect('/login');
+  return next();
 };
 
 const isCampgroundAuthor = async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
 
-  if (campground.author._id.toString() === req.session.user_id) {
-    return next();
+  if (campground.author._id.toString() !== req.session.user_id) {
+    req.flash('msg', {
+      type: 'info',
+      message: 'Unauthorized access, You are not the author of this campground',
+    });
+
+    res.redirect(`/campgrounds/${campground._id}`);
   }
 
-  req.flash('msg', {
-    type: 'info',
-    message: 'Unauthorized access, You are not the author of this campground',
-  });
-
-  res.redirect(`/campgrounds/${campground._id}`);
+  return next();
 };
 
 const isReviewAuthor = async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
 
-  if (campground.author._id.toString() === req.session.user_id) {
-    return next();
-  }
-
   const review = campground.reviews.find(
     (review) => review._id.toString() === req.params.reviewId
   );
 
-  if (review === req.session.user_id) {
-    return next();
+  if (
+    campground.author._id.toString() !== req.session.user_id &&
+    review !== req.session.user_id
+  ) {
+    req.flash('msg', {
+      type: 'info',
+      message: 'Unauthorized access, You are not the author of this review',
+    });
+
+    res.redirect(`/campgrounds/${campground._id}`);
   }
 
-  req.flash('msg', {
-    type: 'info',
-    message: 'Unauthorized access, You are not the author of this review',
-  });
-
-  res.redirect(`/campgrounds/${campground._id}`);
+  return next();
 };
 
 export { isLoggedIn, isCampgroundAuthor, isReviewAuthor };
