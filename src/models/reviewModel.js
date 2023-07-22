e40f06cb9661ce6ad2801f682f62a457e7261077
Nitzan <nitzantomer1998@gmail.com>
2023-07-22 07:14:26 +0300

    Updating Structure

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -3,11 +3,7 @@ import mongoose from 'mongoose';
 const reviewSchema = new mongoose.Schema({
   rating: { type: Number, required: true },
   body: { type: String, required: true },
-
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
 const Review = mongoose.model('Review', reviewSchema);
