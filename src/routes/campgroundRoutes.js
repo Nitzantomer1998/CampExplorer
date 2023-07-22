68566436313a300ca82402c5f073c085ae70ae49
Nitzan <nitzantomer1998@gmail.com>
2023-07-22 09:49:53 +0300

    Updating Structure

--- src/routes/campgroundRoutes.js
+++ src/routes/campgroundRoutes.js
@@ -1,8 +1,8 @@
 import multer from 'multer';
 import express from 'express';
 import wrapAsync from '../utils/wrapAsync.js';
-import { cloudinaryStorage } from '../configs/cloudinaryConfig.js';
 import { validateCampground } from '../utils/validations.js';
+import { cloudinaryStorage } from '../configs/cloudinaryConfig.js';
 import { isLoggedIn, isCampgroundAuthor } from '../middlewares/AuthorizationMiddleware.js';
 import {
   getAllCampgroundsPage,
