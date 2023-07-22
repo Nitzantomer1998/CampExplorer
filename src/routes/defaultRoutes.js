b1d0efa3307f804e28c575d062d16380da131deb
Nitzan <nitzantomer1998@gmail.com>
2023-07-22 09:50:47 +0300

    Adding defualt routes file

---
+++ src/routes/defaultRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import wrapAsync from '../utils/wrapAsync.js';
+
+const router = express.Router();
+
+router.get('/', wrapAsync(async (req, res) => { res.render('home') }));
+
+export default router;
