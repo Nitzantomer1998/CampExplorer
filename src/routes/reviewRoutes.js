71c65d7b6e21fad852f17eedf149a11be3bdd92d
Nitzan <nitzantomer1998@gmail.com>
2023-07-22 07:05:29 +0300

    Updating Structure

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,26 +1,13 @@
 import express from 'express';
 import wrapAsync from '../utils/wrapAsync.js';
 import { validateReview } from '../utils/validations.js';
-import {
-  isLoggedIn,
-  isReviewAuthor,
-} from '../middlewares/AuthorizationMiddleware.js';
 import { postReview, deleteReview } from '../controllers/reviewController.js';
+import { isLoggedIn, isReviewAuthor } from '../middlewares/AuthorizationMiddleware.js';
 
 const router = express.Router();
 
-router.post(
-  '/campgrounds/:id/reviews',
-  isLoggedIn,
-  validateReview,
-  wrapAsync(postReview)
-);
+router.post('/campgrounds/:id/reviews', isLoggedIn, validateReview, wrapAsync(postReview));
 
-router.delete(
-  '/campgrounds/:id/reviews/:reviewId',
-  isLoggedIn,
-  isReviewAuthor,
-  wrapAsync(deleteReview)
-);
+router.delete('/campgrounds/:id/reviews/:reviewId', isLoggedIn, isReviewAuthor, wrapAsync(deleteReview));
 
 export default router;
