fcf36137777cff221957bfa91613f72ab4d7b1e3
Nitzan <nitzantomer1998@gmail.com>
2023-07-22 07:31:36 +0300

    Updating Structure

--- src/middlewares/authorizationMiddleware.js
+++ src/middlewares/authorizationMiddleware.js
@@ -2,11 +2,7 @@ import Campground from '../models/campgroundModel.js';
 
 const isLoggedIn = (req, res, next) => {
   if (!req.session.user_id) {
-    req.flash('msg', {
-      type: 'info',
-      message: 'Unauthorized access, Login first',
-    });
-
+    req.flash('msg', { type: 'info', message: 'Unauthorized access, Login first' });
     res.redirect('/login');
   }
 
@@ -17,11 +13,7 @@ const isCampgroundAuthor = async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
 
   if (campground.author._id.toString() !== req.session.user_id) {
-    req.flash('msg', {
-      type: 'info',
-      message: 'Unauthorized access, You are not the author of this campground',
-    });
-
+    req.flash('msg', { type: 'info', message: 'Unauthorized access, You are not the author of this campground' });
     res.redirect(`/campgrounds/${campground._id}`);
   }
 
@@ -31,19 +23,10 @@ const isCampgroundAuthor = async (req, res, next) => {
 const isReviewAuthor = async (req, res, next) => {
   const campground = await Campground.findById(req.params.id);
 
-  const review = campground.reviews.find(
-    (review) => review._id.toString() === req.params.reviewId
-  );
+  const review = campground.reviews.find((review) => review._id.toString() === req.params.reviewId);
 
-  if (
-    campground.author._id.toString() !== req.session.user_id &&
-    review !== req.session.user_id
-  ) {
-    req.flash('msg', {
-      type: 'info',
-      message: 'Unauthorized access, You are not the author of this review',
-    });
-
+  if (campground.author._id.toString() !== req.session.user_id && review !== req.session.user_id) {
+    req.flash('msg', { type: 'info', message: 'Unauthorized access, You are not the author of this review' });
     res.redirect(`/campgrounds/${campground._id}`);
   }
 
