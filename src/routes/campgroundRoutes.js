49c3adde35b5f088511f1112354549858176e30e
Nitzan <nitzantomer1998@gmail.com>
2023-07-22 07:10:57 +0300

    Updating Structure

--- src/routes/campgroundRoutes.js
+++ src/routes/campgroundRoutes.js
@@ -1,12 +1,9 @@
-import express from 'express';
 import multer from 'multer';
+import express from 'express';
 import wrapAsync from '../utils/wrapAsync.js';
-import cloudinaryStorage from '../configs/cloudinaryConfig.js';
+import { cloudinaryStorage } from '../configs/cloudinaryConfig.js';
 import { validateCampground } from '../utils/validations.js';
-import {
-  isLoggedIn,
-  isCampgroundAuthor,
-} from '../middlewares/AuthorizationMiddleware.js';
+import { isLoggedIn, isCampgroundAuthor } from '../middlewares/AuthorizationMiddleware.js';
 import {
   getAllCampgroundsPage,
   getNewCampgroundPage,
@@ -18,41 +15,17 @@ import {
 } from '../controllers/campgroundControllers.js';
 
 const uploadImages = multer({ storage: cloudinaryStorage }).array('images', 5);
-
 const router = express.Router();
 
 router.get('/campgrounds', isLoggedIn, wrapAsync(getAllCampgroundsPage));
 router.get('/campgrounds/new', isLoggedIn, wrapAsync(getNewCampgroundPage));
 router.get('/campgrounds/:id', isLoggedIn, wrapAsync(getCampgroundPage));
-router.get(
-  '/campgrounds/:id/edit',
-  isLoggedIn,
-  isCampgroundAuthor,
-  wrapAsync(getEditCampgroundPage)
-);
+router.get('/campgrounds/:id/edit', isLoggedIn, isCampgroundAuthor, wrapAsync(getEditCampgroundPage));
 
-router.post(
-  '/campgrounds',
-  isLoggedIn,
-  uploadImages,
-  validateCampground,
-  wrapAsync(postCampground)
-);
+router.post('/campgrounds', isLoggedIn, uploadImages, validateCampground, wrapAsync(postCampground));
 
-router.put(
-  '/campgrounds/:id',
-  isLoggedIn,
-  isCampgroundAuthor,
-  uploadImages,
-  validateCampground,
-  wrapAsync(postUpdatedCampground)
-);
+router.put('/campgrounds/:id', isLoggedIn, isCampgroundAuthor, uploadImages, validateCampground, wrapAsync(postUpdatedCampground));
 
-router.delete(
-  '/campgrounds/:id',
-  isLoggedIn,
-  isCampgroundAuthor,
-  wrapAsync(deleteCampground)
-);
+router.delete('/campgrounds/:id', isLoggedIn, isCampgroundAuthor, wrapAsync(deleteCampground));
 
 export default router;
