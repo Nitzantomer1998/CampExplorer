de40420edab446a4c8fca6bb9642770f3a7af6ac
Nitzan <nitzantomer1998@gmail.com>
2023-07-23 10:40:09 +0300

    Linter Enhance

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -7,18 +7,18 @@ const postReview = async (req, res) => {
     author: req.session.user_id,
   }).save();
 
-  const campground = await Campground.findByIdAndUpdate(req.params.id, { $push: { reviews: review }});
+  const campground = await Campground.findByIdAndUpdate(req.params.id, { $push: { reviews: review } });
 
   req.flash('msg', { type: 'success', message: 'Successfully made a review!' });
   res.redirect(`/campgrounds/${campground._id}`);
 };
 
 const deleteReview = async (req, res) => {
-  await Campground.findByIdAndUpdate(req.params.id, { $pull: { reviews: req.params.reviewId }});
+  await Campground.findByIdAndUpdate(req.params.id, { $pull: { reviews: req.params.reviewId } });
 
   await Review.findByIdAndDelete(req.params.reviewId);
 
-  req.flash('msg', {type: 'success',message: 'Successfully deleted a review!'});
+  req.flash('msg', { type: 'success', message: 'Successfully deleted a review!' });
   res.redirect(`/campgrounds/${req.params.id}`);
 };
 
