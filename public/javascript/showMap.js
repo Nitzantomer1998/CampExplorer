f08d664ad1a5f1dd242d699bde5a1cb8d9e521a5
Nitzan <nitzantomer1998@gmail.com>
2023-07-23 10:34:15 +0300

    Linter Enhance

--- public/javascript/showMap.js
+++ public/javascript/showMap.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
@@ -11,12 +12,11 @@ map.addControl(new mapboxgl.NavigationControl());
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(
-    new mapboxgl.Popup({ offset: 25 })
-    .setHTML(
-      "<p><strong><a href='/campgrounds/" + campground._id + "'>" + campground.title + "</a></strong><p>" + 
-            "<p>" + campground.location + "</p>" +
-            "<p>" + campground.description.substring(0, 20) + "...</p>" +
-            "<img src='" + campground.images[0].url + "' width='175px' height='100' />" 
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<p><strong><a href='/campgrounds/${campground._id}'>${campground.title}</a></strong></p>
+      <p>${campground.location}</p>
+      <p>${campground.description.substring(0, 20)}...</p>
+      <img src='${campground.images[0].url}' width='175px' height='100' />`
     )
   )
   .addTo(map);
